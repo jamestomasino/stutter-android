58f47feb104bb08d9cd42e630d21c54d1ffc47db
James Tomasino <james@tomasino.org>
2026-01-23 12:51:18 +0000

    upgrades gradle settings to match fdroid deploy

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "org.tomasino.stutter"
         minSdk = 24
         targetSdk = 34
-        versionCode = 10
-        versionName = "0.3.0"
+        versionCode = 11
+        versionName = "1.0.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
